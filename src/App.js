066cf295019f2b69c36ab7b46d8c94ff50afb6b4
Giovanne-Oliveira-png <69925182+Giovanne-Oliveira-png@users.noreply.github.com>
2021-06-29 11:22:14 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './estoque.png';
+import './App.css';
+import React from 'react';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Estoque Online</code> 
+        </p>
+        <a
+          className="App-link"
+          href="https://estoqueonline.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
